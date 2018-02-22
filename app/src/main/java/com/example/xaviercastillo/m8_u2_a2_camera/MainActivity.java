a42f87cf4b6cde706af372e125fcb30b3d5c6ba6
Xavier Castillo <smxxcastillo@gmail.com>
2018-02-22 16:00:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/xaviercastillo/m8_u2_a2_camera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xaviercastillo.m8_u2_a2_camera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
